45754fc417f45bf0e778791fe301290304a1150d
Michal Moudrý <moudry.michal@live.com>
2025-11-09 20:12:30 +0100

    gtk section

--- gtk/main.cpp
+++ gtk/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
 int main(int arc, char* argv[]) {
+    std::cout << "Test" << std::endl;
+    char test[3] = {'1', '2', '3'};
+    for (auto& ch : test) {
+        std::cout << ch << std::endl;
+    }
     return 0;
 }
